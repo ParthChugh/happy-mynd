c074fa6b4561afe464c7c7736450e9d6b48c170d
Parth Chugh <parthchugh52@gmail.com>
2021-02-09 00:51:46 +0530

    added some part of it

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/logo-with-name.png';
+import emotions from './assets/emotions.png';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      
+      <div className="d-flex justify-content-between">
+        <div className="d-flex flex-column ">
+          <div>
+            <img src={logo} className="App-logo" alt="logo" />
+          </div>
+          <div className="d-flex flex-column">
+            <span className="App-header">Life is filled with <span style={{color: '#FF5757'}}>Happiness</span> Lets explore it !</span>
+            <span className="App-sub-heading">We provide you a Mentorship program to make your life better and help you to discover the positive inner side of you</span>
+            <div>
+            <button className="global-button"><span>Explore more</span></button>
+            </div>
+            
+          </div>
+          
+        </div>
+        <img src={emotions} alt="logo" />
+      </div>
+      
     </div>
   );
 }
